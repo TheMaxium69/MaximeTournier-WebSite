a0473ddd72c4ddd1e38f8b9f8536f1ac59ebb67a
Marilyne Naili <marilyne.lydia@gmail.com>
2023-10-25 22:17:04 +0200

    update navbar hover

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ head($page); ?>
             
             <!-- COMPETENCES -->
             <section id="competence">
-                <h2 class="h2 text-center">Les compétences acquissent</h2>
+                <h2 class="h2 text-center">Les compétences acquises</h2>
                 <div class="content">
                     <div class="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
                         <div class="group-hover:duration-400 relative rounded-2xl w-72 h-36 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-72 before:h-32 before:-z-10">
@@ -80,19 +80,7 @@ head($page); ?>
                         </div>
                     </div>
             
-                    <div class="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
-                        <div class="group-hover:duration-400 relative rounded-2xl w-72 h-36 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-72 before:h-32 before:-z-10">
-                            <span class="text-3xl font-bold">Formateur</span>
-                            <p class="text-amber-300 font-thin">- Date : 2019 -</p>
-                        </div>
-                    </div>
-            
-                    <div class="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
-                        <div class="group-hover:duration-400 relative rounded-2xl w-72 h-36 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-72 before:h-32 before:-z-10">
-                            <span class="text-3xl font-bold">Formateur</span>
-                            <p class="text-amber-300 font-thin">- Date : 2019 -</p>
-                        </div>
-                    </div>
+                    
                 </div>
             </section>
             <!-- /COMPETENCES -->
