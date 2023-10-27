fca05a162ee7499300a2ec5966a8ef2bb2c8723a
Marilyne Naili <marilyne.lydia@gmail.com>
2023-10-27 09:32:33 +0200

    test fixe bug git

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ head($page); ?>
 
 <body>
     <header> <?php navbar($page); ?> </header>
-    
+
     <!-- BACKGROUND -->
     <!-- <canvas class="full-page-bg orb-canvas"></canvas> -->
     <!-- /BACKGROUND -->
-    
-    
-    <main >
-        
+
+
+    <main>
+
         <!-- HEADER -->
         <section id="header">
             <div class="card">
@@ -28,17 +28,17 @@ head($page); ?>
                         </blockquote>
                     </div>
                     <div class="card-picture">
-                        </div>
                     </div>
                 </div>
             </div>
+            </div>
         </section>
         <!-- /HEADER -->
-        
-        
+
+
         <!-- COMPETENCES -->
         <section id="competence">
-            <h2 class="h2 text-center">Les compétences acquises</h2>
+            <h2 class="h2 text-center">Compétences acquises</h2>
             <div class="content">
                 <div class="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
                     <div class="group-hover:duration-400 relative rounded-2xl w-72 h-36 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-72 before:h-32 before:-z-10">
@@ -46,46 +46,46 @@ head($page); ?>
                         <p class="text-amber-300 font-thin">- Date : 2019 -</p>
                     </div>
                 </div>
-        
+
                 <div class="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
                     <div class="group-hover:duration-400 relative rounded-2xl w-72 h-36 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-72 before:h-32 before:-z-10">
                         <span class="text-3xl font-bold">Management</span>
                         <p class="text-amber-300 font-thin">- Date : 2019 -</p>
                     </div>
                 </div>
-        
+
                 <div class="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
                     <div class="group-hover:duration-400 relative rounded-2xl w-72 h-36 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-72 before:h-32 before:-z-10">
                         <span class="text-3xl font-bold">Formateur</span>
                         <p class="text-amber-300 font-thin">- Date : 2019 -</p>
                     </div>
                 </div>
-        
+
                 <div class="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
                     <div class="group-hover:duration-400 relative rounded-2xl w-72 h-36 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-72 before:h-32 before:-z-10">
                         <span class="text-3xl font-bold">Gestion Projet</span>
                         <p class="text-amber-300 font-thin">- Date : 2019 -</p>
                     </div>
                 </div>
-        
+
                 <div class="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
                     <div class="group-hover:duration-400 relative rounded-2xl w-72 h-36 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-72 before:h-32 before:-z-10">
                         <span class="text-3xl font-bold">Management</span>
                         <p class="text-amber-300 font-thin">- Date : 2019 -</p>
                     </div>
                 </div>
-        
+
                 <div class="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
                     <div class="group-hover:duration-400 relative rounded-2xl w-72 h-36 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-72 before:h-32 before:-z-10">
                         <span class="text-3xl font-bold">Formateur</span>
                         <p class="text-amber-300 font-thin">- Date : 2019 -</p>
                     </div>
                 </div>
-        
-                
+
+
             </div>
         </section>
-            <!-- /COMPETENCES -->
+        <!-- /COMPETENCES -->
 
         <!-- PROJETS -->
         <section id="project">
@@ -114,7 +114,7 @@ head($page); ?>
                             </div>
                             <div class="text">
                                 <h3 class="h3">Formateur</h3>
-                                
+
                                 <p>Premier mouvement français pour défendre la nouvelle génération d'entrepreneur, de créateur de contenu, d'artiste et métiers d'internet.</p>
                             </div>
                         </div>
@@ -192,71 +192,71 @@ head($page); ?>
         <!-- /SERVICES -->
 
         <!-- Footer -->
-<footer id="footer">
+        <footer id="footer">
 
-  <!-- Section: Links  -->
-  <section class="">
-    <div class="container text-center text-md-start mt-5">
-      <!-- Grid row -->
-      <div class="row mt-3">
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <!-- Content -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>MAXIME TOURNIER
-          </h6>
-          <p>
-            Here you can use rows and columns to organize your footer content. Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit.
-          </p>
-        </div>
-        <!-- Grid column -->
+            <!-- Section: Links  -->
+            <section class="">
+                <div class="container text-center text-md-start mt-5">
+                    <!-- Grid row -->
+                    <div class="row mt-3">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <!-- Content -->
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                <i class="fas fa-gem me-3"></i>MAXIME TOURNIER
+                            </h6>
+                            <p>
+                                Here you can use rows and columns to organize your footer content. Lorem ipsum
+                                dolor sit amet, consectetur adipisicing elit.
+                            </p>
+                        </div>
+                        <!-- Grid column -->
 
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Pages
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">A propos</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Expériences</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Compétences</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Histoire</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Services</a>
-          </p>
-        </div>
-        <!-- Grid column -->
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                Pages
+                            </h6>
+                            <p>
+                                <a href="#!" class="text-reset">A propos</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset">Expériences</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset">Compétences</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset">Histoire</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset">Services</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
 
-        <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-          <p><i class="fas fa-home me-3"></i> Décines Chapieu</p>
-          <p>
-            <i class="fas fa-envelope me-3"></i>
-            mxmtournier@gmail.com
-          </p>
-          <p><i class="fas fa-phone me-3"></i>06.11.11.26.69</p>
-        </div>
-        <!-- Grid column -->
-      </div>
-      <!-- Grid row -->
-    </div>
-  </section>
-  <!-- Section: Links  -->
-</footer>
-<!-- Footer -->
+                        <!-- Grid column -->
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                            <p><i class="fas fa-home me-3"></i> Décines Chapieu</p>
+                            <p>
+                                <i class="fas fa-envelope me-3"></i>
+                                mxmtournier@gmail.com
+                            </p>
+                            <p><i class="fas fa-phone me-3"></i>06.11.11.26.69</p>
+                        </div>
+                        <!-- Grid column -->
+                    </div>
+                    <!-- Grid row -->
+                </div>
+            </section>
+            <!-- Section: Links  -->
+        </footer>
+        <!-- Footer -->
 
-        
+
 
     </main>
     <script src="https://cdn.tailwindcss.com"></script>
