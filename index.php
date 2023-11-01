37c5b330a662514b4f78fd0aff1a92282907f0a6
TheMaximeSan <mxm.tournier@gmail.com>
2023-11-01 18:42:30 +0100

    update

--- index.php
+++ index.php
@@ -91,23 +91,23 @@ zaekzazan lenzaln ezalkenzaljne zabezkjbezae
 
                         <div class="left card-white card-style">
 
-                            <article>
+                            <article class="tyro-art">
 
                                 <div class="img img-tyro"></div>
 
                                 <h3>Tyrolium</h3>
                                 <p style="margin-bottom: unset"><small>Fondé en Juillet 2017</small></p>
                                 <hr style="
-                            margin: 13px auto!important;
+                            margin: 35px auto!important;
                             width: 60%!important;
                             border: solid white 1px!important;
                             opacity: 0.7!important;">
                                 <p class="projet-desc desc-tyro">
                                     J'ai fondé ce projet en 2017 à l'âge de 14ans et continue toujours aujourd'hui
                                     C'est littéralement le projet de toute une vie pour moi !!
-<br><br>
+                                    <br><br>
                                     Mais c'est quoi ?
-<br><br>
+                                    <br><br>
                                     Tyrolium est une entreprise multisectorielle qui travaille sur plusieurs projets, principalement sur du développement Web mais aussi notamment du développement de jeux vidéos ou d'applications, de la musique, ou de l'audiovisuel.
 
 
@@ -147,10 +147,30 @@ zaekzazan lenzaln ezalkenzaljne zabezkjbezae
                             <article class="d-flex Art-other">
 
                                 <div class="left-auto">
-                                    <div class="img img-tyro"></div>
+                                    <div class="img img-gen"></div>
                                 </div>
                                 <div class="right">
-                                    <h3>Tyrolium</h3>
+                                    <h3>Génération IUM</h3>
+                                    <p style="margin-bottom: unset"><small>Fondé en Juin 2022</small></p>
+                                    <hr style="
+                            margin: 13px auto!important;
+                            width: 60%!important;
+                            border: solid white 1px!important;
+                            opacity: 0.7!important;">
+                                    <p class="projet-desc">
+                                        j'ai fondé cette organisation politique qui a pour but de défendre la nouvelle génération d'entrepreneur, de créateur de contenu, d'artiste et métiers d'internet.
+                                    </p>
+                                </div>
+
+                            </article>
+                            <hr>
+                            <article class="d-flex Art-other">
+
+                                <div class="left-auto">
+                                    <div class="img img-doublet"></div>
+                                </div>
+                                <div class="right">
+                                    <h3>DoubleT</h3>
                                     <p style="margin-bottom: unset"><small>Créer le</small></p>
                                     <hr style="
                             margin: 13px auto!important;
@@ -163,12 +183,68 @@ zaekzazan lenzaln ezalkenzaljne zabezkjbezae
                             </article>
 
                         </div>
-
                     </div>
 
 
 
+                    <div class="d-flex">
+
+
+                        <div class="left left-down card-white card-style">
+
+                            <article class="d-flex Art-other">
+
+                                <div class="left-auto">
+                                    <div class="img img-max-mc"></div>
+                                </div>
+                                <div class="right">
+                                    <h3>Maxime/Minecraft</h3>
+                                    <p style="margin-bottom: unset"><small>Fondé en Juin 2022</small></p>
+                                    <hr style="
+                            margin: 13px auto!important;
+                            width: 60%!important;
+                            border: solid white 1px!important;
+                            opacity: 0.7!important;">
+                                    <p class="projet-desc">
+                                        j'ai fondé cette organisation politique qui a pour but de défendre la nouvelle génération d'entrepreneur, de créateur de contenu, d'artiste et métiers d'internet.
+                                    </p>
+                                </div>
+
+                            </article>
+
+                        </div>
+                        <div class="right card-white card-style">
+
+                            <article class="d-flex Art-other">
+
+                                <div class="left-auto">
+                                    <div class="img img-max-serv"></div>
+                                </div>
+                                <div class="right">
+                                    <h3>Maxime/Serveur</h3>
+                                    <p style="margin-bottom: unset"><small>Fondé en Juin 2022</small></p>
+                                    <hr style="
+                            margin: 13px auto!important;
+                            width: 60%!important;
+                            border: solid white 1px!important;
+                            opacity: 0.7!important;">
+                                    <p class="projet-desc">
+                                        j'ai fondé cette organisation politique qui a pour but de défendre la nouvelle génération d'entrepreneur, de créateur de contenu, d'artiste et métiers d'internet.
+                                    </p>
+                                </div>
+
+                            </article>
+
+                        </div>
+
+                    </div>
+
+
                     <style>
+
+                        #project div.d-flex{
+                            margin-top: 15px;
+                        }
 
                         #project div.img{
                             margin: 14px auto;
@@ -177,27 +253,18 @@ zaekzazan lenzaln ezalkenzaljne zabezkjbezae
                             background-size: contain;
                         }
 
-                        .img-tyro{
-                            width: 229px;
-                            height: 177px;
-                            background-image: url("https://tyrolium.fr/Contenu/Image/Tyrolium Site.png");
-                        }
-                        .img-gen{
-                            border-radius: 33px;
-                            width: 200px;
-                            height: 200px;
-                            background-image: url("https://tyrolium.fr/assets/part/Generatium-ium.png");
-                            background-size: cover !important;
-                        }
+
 
                         #project div.right hr{
                             width : 85%;
                         }
 
-                        #project div.right div.right{
+                        #project div.right div.right,
+                        #project div.left-down div.right{
                             width : 50%;
                         }
-                        #project div.right div.left-auto{
+                        #project div.right div.left-auto,
+                        #project div.left-down div.left-auto{
                             margin-left: auto !important;
                         }
 
@@ -210,6 +277,53 @@ zaekzazan lenzaln ezalkenzaljne zabezkjbezae
 
                         #project .Art-other{
                             margin: 64px 0px;
+                        }
+
+                        #project .tyro-art{
+                            padding: 30px 0px;
+                        }
+
+
+
+                        .img-tyro{
+                            border-radius: 33px;
+                            width: 200px;
+                            height: 200px;
+                            background-image: url("https://tyrolium.fr/Contenu/Image/Tyrolium.png");
+                            background-size: cover !important;
+                        }
+                        /*.img-tyro{
+                            width: 229px;
+                            height: 177px;
+                            background-image: url("https://tyrolium.fr/Contenu/Image/Tyrolium Site.png");
+                        }*/
+                        .img-gen{
+                            border-radius: 33px;
+                            width: 200px;
+                            height: 200px;
+                            background-image: url("https://tyrolium.fr/assets/part/Generatium-ium.png");
+                            background-size: cover !important;
+                        }
+                        .img-max-mc{
+                            border-radius: 33px;
+                            width: 200px;
+                            height: 200px;
+                            background-image: url("https://tyrolium.fr/assets/part/Maxime-MINECRAFT.png");
+                            background-size: cover !important;
+                        }
+                        .img-max-serv{
+                            border-radius: 33px;
+                            width: 200px;
+                            height: 200px;
+                            background-image: url("https://tyrolium.fr/assets/part/Maxime-SERVEUR.jpg");
+                            background-size: cover !important;
+                        }
+                        .img-doublet{
+                            border-radius: 33px;
+                            width: 200px;
+                            height: 200px;
+                            background-image: url("https://tyrolium.fr/assets/part/DoubleT.png");
+                            background-size: cover !important;
                         }
 
 
