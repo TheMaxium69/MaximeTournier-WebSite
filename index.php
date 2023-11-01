27fe31452f0a8636794e88f62c01e1de6299d5ae
TheMaximeSan <mxm.tournier@gmail.com>
2023-11-01 16:13:20 +0100

    update

--- index.php
+++ index.php
@@ -20,6 +20,15 @@ head($page); ?>
         </section>
         <!-- /HEADER -->
 
+        <!-- COMPETENCES -->
+        <section id="competence">
+            <div class="card">
+                <div class="bg">
+                    <h2>A propos</h2>
+                </div>
+            </div>
+        </section>
+        <!-- /COMPETENCES -->
 
         <!-- COMPETENCES -->
         <section id="competence">
@@ -103,12 +112,84 @@ head($page); ?>
 
         <div class="card card-white">
             <div class="bg">
-                <h2>FOOTER</h2>
+
+                    <!-- Section: Links  -->
+                    <section class="">
+                        <div class="container text-center text-md-start mt-5">
+                            <!-- Grid row -->
+                            <div class="row mt-3">
+                                <!-- Grid column -->
+                                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                                    <!-- Content -->
+                                    <h6 class="text-uppercase fw-bold mb-4">
+                                        <i class="fas fa-gem me-3"></i>MAXIME TOURNIER
+                                    </h6>
+                                    <p>
+                                        Here you can use rows and columns to organize your footer content. Lorem ipsum
+                                        dolor sit amet, consectetur adipisicing elit.
+                                    </p>
+                                </div>
+                                <!-- Grid column -->
+
+                                <!-- Grid column -->
+                                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                    <!-- Links -->
+                                    <h6 class="text-uppercase fw-bold mb-4">
+                                        Pages
+                                    </h6>
+                                    <p>
+                                        <a href="#!" class="text-reset">A propos</a>
+                                    </p>
+                                    <p>
+                                        <a href="#!" class="text-reset">Expériences</a>
+                                    </p>
+                                    <p>
+                                        <a href="#!" class="text-reset">Compétences</a>
+                                    </p>
+                                    <p>
+                                        <a href="#!" class="text-reset">Histoire</a>
+                                    </p>
+                                    <p>
+                                        <a href="#!" class="text-reset">Services</a>
+                                    </p>
+                                </div>
+                                <!-- Grid column -->
+
+                                <!-- Grid column -->
+                                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                                    <!-- Links -->
+                                    <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                                    <p><i class="fas fa-home me-3"></i> Décines Chapieu</p>
+                                    <p>
+                                        <i class="fas fa-envelope me-3"></i>
+                                        mxmtournier@gmail.com
+                                    </p>
+                                    <p><i class="fas fa-phone me-3"></i>06.11.11.26.69</p>
+                                </div>
+                                <!-- Grid column -->
+                            </div>
+                            <!-- Grid row -->
+                        </div>
+                    </section>
+                    <!-- Section: Links  -->
+
+
+
+
+
+
+
+
+
+
             </div>
         </div>
 
     </footer>
     <!-- /FOOTER -->
+
+
+
 
 </body>
 
